acc3cd02c73fd16e966255ce12a2dbffa42ebadd
DaniloMP10 <zdanilo2014@hotmail.com>
2025-04-10 01:10:46 -0300

    feat(theme): adiciona arquivo com paleta de cores do app

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // roxo padrão para o app
+  static const Color primary = Color(0xFF6A1B9A);
+
+  // branco para fundos e outros elementos
+  static const Color background = Colors.white;
+  // preto para textos e detalhes
+  static const Color text = Colors.black;
+}
